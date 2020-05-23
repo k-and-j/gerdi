34a2368d86c29941f36802c0017887f8c489d34b
force <force1267@gmail.com>
2020-05-23 21:52:13 +0430

    first y

--- y.js
+++ y.js
@@ -1,9 +1,21 @@
 use(p => {
     p.color = "aqua"
-    const bullet_speed = 1.5;
+    const bullet_speed = 1.5
+    const op = {
+        dx: 0, dy: 0,
+        px: 0, py: 0
+    }
+    const rnd = (a, b) => Math.random() * (b - a) + a
     p.loop = () => {
-        const {data, move, shot} = p;
+        const {data, move, shoot} = p;
         
+        op.dx = data.ox - op.px
+        op.dy = data.oy - op.py
         
+        move(rnd(100, data.screenwidth - 100) - data.x, rnd(100, data.screenheight - 100) - data.y)
+        shoot(data.ox + op.dx, data.oy + op.dy)
+
+        op.px = data.ox;
+        op.py = data.oy;
     }
 })
